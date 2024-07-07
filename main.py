227259905f5e61ccc8f0c7fb579f92348f2a6f2d
Harsha <harshahm0130@gmail.com>
2024-07-07 20:10:36 +0530

    Updating our exercises to spreadsheet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 API_ID = "92bc5c4e"
 API_KEY = "d324f0d03832da0f828634bf2d28d61f"
@@ -25,5 +26,29 @@ parameters = {
 }
 
 response = requests.post(url=api_endpoint, headers=header, json=parameters)
-print(response.json())
+result = response.json()
+
+sheety_api_endpoint = "https://api.sheety.co/f72c7dde575b917aa0ceee138a8b92fd/workoutsTracking/workouts"
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+today_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_input = {
+        "workout": {
+            "date": today_date,
+            "time": today_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
+    }
+
+    sheet_response = requests.post(url=sheety_api_endpoint, json=sheet_input)
+
+    print(sheet_response.text)
+
+
+
+
 
